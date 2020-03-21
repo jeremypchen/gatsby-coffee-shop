f45ec8735f14719b187c5f2e31c52774bb02cfa9
Jeremy Chen <jeremypchen@gmail.com>
2020-03-21 13:45:11 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { FaGulp } from "react-icons/fa"
+
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,11 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <FaGulp size={60} />
   </Layout>
 )
 
