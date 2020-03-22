b4418e511c6eae6de5c71a9c9f34723ebd498605
Jeremy Chen <jeremypchen@gmail.com>
2020-03-21 21:41:27 -0700

    Background image, footer and title components

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,12 +13,14 @@ import "./bootstrap.min.css"
 import "./layout.css"
 
 import Navbar from "./Globals/Navbar"
+import Footer from "./Globals/Footer"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import { FaGulp } from "react-icons/fa"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp size={60} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      styleClass="default-background"
+    />
+    <Info />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
